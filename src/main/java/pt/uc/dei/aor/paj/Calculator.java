e3fb7a7ca68ff20a6a619efd4dc8bd3eeebd1e7a
njssferreira <njssferreira@gmail.com>
2015-05-05 10:36:48 +0100

    copy calc

--- src/main/java/pt/uc/dei/aor/paj/Calculator.java
+++ src/main/java/pt/uc/dei/aor/paj/Calculator.java
@@ -1,5 +1,100 @@
-package pt.uc.dei.aor.paj;
+package projecto2;
 
-public class Calculator {
+import java.io.Serializable;
 
+import javax.enterprise.context.SessionScoped;
+import javax.faces.event.ValueChangeEvent;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+@Named
+@SessionScoped
+public class Calculator implements Serializable{
+	
+	/**
+	 * 
+	 */
+	//Variable declaration
+	private static final long serialVersionUID = 3883884812159598031L;
+	private String expression;
+	private boolean calculado=false;	
+
+	@Inject History hist;
+	@Inject Statistic stat;
+
+	
+
+	//Constructor
+	public Calculator() {
+		expression="0";
+	}
+	
+	//Returns expression
+	public String getExpression() {
+		return expression;
+	}
+
+	//Expression setter	
+	public void setExpression(String expression) {
+		this.expression = expression;
+	}
+	
+	//Add another part to string (user input)
+	public void add(String srt){
+		if(calculado == true){
+			expression = "0";
+			calculado = false;
+		}
+		if (expression.equals("0")){
+			expression="";
+		}
+		if (expression.length()+srt.length()<25){
+			expression+=srt;
+		}
+	}
+	
+	//Method that receives string from History and adds to expression
+	public void submitOp(ValueChangeEvent vce){
+		if (expression.equals("0")){
+			expression="";
+		}
+		
+		calculado = false;
+		if (expression.length()+(vce.getNewValue().toString().length())<25){
+			expression += vce.getNewValue().toString();
+		}
+	}
+
+	
+	//Clear calc when user hits 'C'
+	public String clearCalc(){
+		expression="0";
+		return expression;
+	}
+	
+	//Calculate the expression using exp4j
+	public void calcExp(){
+		try{
+			Expression e = new ExpressionBuilder(expression)
+			.variables("pi", "e")
+			.build()
+			.setVariable("pi", Math.PI)
+			.setVariable("e", Math.E);
+			String aux = expression;
+			try{
+				expression = String.valueOf(e.evaluate());
+				hist.addHist(aux);
+				stat.addStat(aux);
+			} catch (Exception exp){
+				expression = exp.getMessage();
+			}	
+		} catch (Exception exp){
+			expression = exp.getMessage();
+		}	
+		calculado=true;
+	}
+	
 }
---
+++ src/main/java/pt/uc/dei/aor/paj/History.java
@@ -0,0 +1,50 @@
+package projecto2;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class History implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7514545189871598589L;
+	ArrayList<String> lista;
+	
+	
+	//Constructor
+	public History(){
+		lista = new ArrayList<String>();
+	}
+
+	//ArrayList with historic getter
+	public ArrayList<String> getLista() {
+		return lista;
+	}
+	
+	//ArrayList with historic setter
+	public void setLista(ArrayList<String> lista) {
+		this.lista = lista;
+	}
+	
+	//Adds another expression to historic ArrayList
+	public void addHist(String srt){
+		boolean exists=false;
+		for (String x: lista){
+			if (x.equals(srt)){
+				exists=true;
+			}
+		}
+		if (!exists){
+			lista.add(srt);
+		}
+	}
+	
+	
+	
+	
+	
+}
