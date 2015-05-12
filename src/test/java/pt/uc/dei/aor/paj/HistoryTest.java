90546bd5dc32218110ee14011e9ae8a3bf39f414
njssferreira <njssferreira@gmail.com>
2015-05-12 17:04:39 +0100

    history test

--- src/test/java/pt/uc/dei/aor/paj/HistoryTest.java
+++ src/test/java/pt/uc/dei/aor/paj/HistoryTest.java
@@ -14,11 +14,15 @@ public class HistoryTest {
 	
 	private History history;
 	private Screen screen;
+	private Calculator calculator;
+	private HistoryEntry historyentry;
 	
 	@Before
 	public void init() {
 		history = new History();
 		screen = new Screen();
+		calculator = new Calculator();
+		
 	}
 	
 	@Test
@@ -29,5 +33,24 @@ public class HistoryTest {
 		screen.concat("2");
 		history.addEntry(screen, "4", 100);
 		assertThat(history.getListEntry().size(), is(equalTo(size+1)));
+	}
+	
+	@Test
+	public void should_add_reused_result_to_top_of_list(){
+		int size = history.getListEntry().size();
+		screen.concat("1");
+		screen.concat("+");
+		screen.concat("2");
+		history.addEntry(screen, "3", 100);
+		screen = new Screen();
+		screen.concat("5");
+		screen.concat("+");
+		screen.concat("2");
+		history.addEntry(screen, "7", 100);
+		assertThat(history.getListEntry().size(), is(equalTo(2)));
+		history.addEntry(history.getListEntry().get(1).getExpression(), "3", 234L);
+		assertThat(history.getListEntry().size(), is(equalTo(size+2)));
+		
+		
 	}
 }
